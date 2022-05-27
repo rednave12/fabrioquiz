13ee54aa19f86f7c1d5925ab4e35a03e63a97ce7
Evander Turner <evanderturner97@gmail.com>
2022-05-27 12:19:13 +0100

    added comments & cleaned up a bit

--- src/App.js
+++ src/App.js
@@ -5,13 +5,27 @@ import data from './data.json';
 
 function App() {
     const [selectedAnswers, setSelectedAnswers] = useState([]);
+    // revealed and correct could be merged in to one state, null if not revealed and true/false for correctness. I've left them separate for clarity.
     const [revealed, setRevealed] = useState(false);
+    const [correct, setCorrect] = useState(null);
 
-    const [correct, setCorrect] = useState(null)
+    const checkAnswer = () => {
+        const correctAnswers = data.data.getStep.stepQuiz.answerOptions.filter(answer => answer.isCorrect === 'true');
+        setRevealed(true);
 
+        // in order to compare selectedAnswers to correctAnswers (both arrays of objects), we sort them both alphabetically and compare the result stringified!
+        if (JSON.stringify(selectedAnswers.sort((a, b) => a.answerText.localeCompare(b.answerText))) === JSON.stringify(correctAnswers.sort((a, b) => a.answerText.localeCompare(b.answerText)))) {
+            setCorrect(true);
+        } else {
+            setCorrect(false);
+        }
+    }
+
+    // I prefer to keep main components as semantic as possible, made up mostly of descriptive components so the app structure is clear.
     return (
         <div className="app">
             <Quiz>
+
                 <QuizHeader>
                     <QuestionNumber number={parseInt(data.data.getStep.id.split('-')[2])} />
                     <Question>{data.data.getStep.stepQuiz.questionText}</Question>
@@ -19,30 +33,22 @@ function App() {
 
                 <QuizMain>
                     <Options>
+                        
                         {data.data.getStep.stepQuiz.answerOptions.map((option, i) => {
+
                             return <div className={`option ${selectedAnswers.includes(option) ? 'selected' : ''} ${revealed ? (option.isCorrect === 'true' ? 'correct' : 'incorrect') : ''}`} key={i}>
                                 <CheckBox revealed={revealed} option={option} selectedAnswers={selectedAnswers} setSelectedAnswers={setSelectedAnswers} />
                                 {option.answerText}
                             </div>
+
                         })}
+
                     </Options>
-                    <Checker selectedAnswers={selectedAnswers} onClick={() => {
-                        const correctAnswers = data.data.getStep.stepQuiz.answerOptions.filter(answer => answer.isCorrect === 'true');
-                        setRevealed(true);
-
-                        console.log(selectedAnswers, correctAnswers);
-                        if (JSON.stringify(selectedAnswers.sort((a, b) => a.answerText.localeCompare(b.answerText))) === JSON.stringify(correctAnswers.sort((a, b) => a.answerText.localeCompare(b.answerText)))) {
-                            console.log('well done');
-                            setCorrect(true);
-                            // TODO: add toasts here?
-                        } else {
-                            console.log('try again');
-                            setCorrect(false);
-                        }
-
-                    }} />
+                    <CheckerButton selectedAnswers={selectedAnswers} onClick={checkAnswer} />
                 </QuizMain>
+
             </Quiz>
+
             <Toast correct={correct} />
         </div>
     );
@@ -91,35 +97,40 @@ function Options(props) {
     )
 }
 
-function Checker(props) {
+function CheckerButton(props) {
     return (
         <button onClick={props.onClick}>Check</button>
     )
 }
 
 function CheckBox(props) {
+    // controlled component.
     const [checked, setChecked] = useState(false);
 
+    const handleCheckboxChange = (e) => {
+        if (props.revealed) return;
+        if (!checked) {
+            setChecked(true);
+            props.setSelectedAnswers([...props.selectedAnswers, props.option]);
+        } else {
+            setChecked(false);
+            // if (props.selectedAnswers.includes(props.option)) {
+            props.setSelectedAnswers(props.selectedAnswers.filter(x => x !== props.option));
+            // }
+        }
+    }
+
     return (
-        <input type="checkbox" onChange={e => {
-            if (props.revealed) return;
-            if (e.target.checked) {
-                setChecked(true)
-                props.setSelectedAnswers([...props.selectedAnswers, props.option]);
-            } else {
-                setChecked(false);
-                if (props.selectedAnswers.includes(props.option)) {
-                    props.setSelectedAnswers(props.selectedAnswers.filter(x => x !== props.option));
-                }
-            }
-        }} checked={checked} />
+        <input type="checkbox" onChange={handleCheckboxChange} checked={checked} />
     )
 }
 
 function Toast(props) {
     return (
         <div className={`toast ${props.correct !== null ? 'active' : ''} ${props.correct ? 'correct' : 'incorrect'}`}>
-            {props.correct !== null ? (props.correct ? 'Correct! ✅' : 'Not quite 😔') : ''}
+            {props.correct !== null ?
+                (props.correct ? 'Correct! ✅' : 'Not quite 😔')
+            : ''}
         </div>
     )
 }
